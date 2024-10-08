04300357ab018d1e931f34472fd85d7c8e6cb629
lzx8035 <lzx8035@gmail.com>
2024-10-07 23:16:16 -0700

    Add 2 Samples of Advanced React Patterns

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// Show sample
+// import App from "./Samples/SampleApp2";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
