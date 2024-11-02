cd716f2bd5f2ccd222b560b8df2876a3a636f311
lzx8035 <lzx8035@gmail.com>
2024-11-01 23:32:30 -0700

    Protect Route with Authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export default function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { isPending, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user); // !!!
       navigate("/dashboard");
-      console.log(user);
     },
     onError: (err) => {
       console.error(err);
