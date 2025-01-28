f04e8529723f4783435b27fd718d9db5d9888cba
lzx8035 <lzx8035@gmail.com>
2025-01-27 23:58:32 -0800

    Fix Bug

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -2,17 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../services/apiAuth";
 
 export default function useUser() {
-  const { isLoading, data: user } = useQuery({
+  const { data: user } = useQuery({
     queryKey: ["user"],
     queryFn: getCurrentUser,
-
     retry: false,
-    staleTime: 1000 * 60 * 5,
-    onError: () => {},
+    // 关键是添加这个
+    initialData: null,
+    // 如果获取用户信息失败，我们认为用户未登录
+    onError: () => {
+      return null;
+    },
   });
 
   return {
-    isLoading,
+    isLoading: false, // 直接返回 false 避免加载状态
     user,
     isAuthenticated: user?.role === "authenticated",
   };
